d6699b930b9545e887813ba06a17ee2ab54c2cc4
sonalshreya25 <sonal.shreya18@gmail.com>
2025-09-30 21:57:51 -0400

    Adding main and function calls

--- Library.cs
+++ Library.cs
@@ -1,88 +1,152 @@
 ﻿using System;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace USFLibraryMgmtSystem
+
 {
+
     internal class Library
+
     {
-        public class library
+
+        // Collections to store books and patrons
+
+        public List<Book> Books { get; set; }
+
+        public List<Person> Patrons { get; set; }
+
+        // Constructor
+
+        public Library()
+
         {
-            // Collections to store books and patrons
-            public List<Book> Books { get; set; }
-            public List<Person> Patrons { get; set; }
 
-            // Constructor
-            public library()
-            {
-                Books = new List<Book>();
-                Patrons = new List<Person>();
-            }
+            Books = new List<Book>();
 
-            // Add a book to the library
-            public void AddBook(Book book)
-            {
-                Books.Add(book);
-            }
+            Patrons = new List<Person>();
 
-            // Add a patron (student/staff) to the library
-            public void AddPatron(Person patron)
-            {
-                Patrons.Add(patron);
-            }
-
-            // Display all books in the library
-            public void DisplayBooks()
-            {
-                Console.WriteLine("\nBooks in Library:");
-                foreach (var book in Books)
-                {
-                    book.DisplayBook();
-                }
-            }
-
-            // Display all patrons in the library
-            public void DisplayPatrons()
-            {
-                Console.WriteLine("\nPatrons in Library:");
-                foreach (var patron in Patrons)
-                {
-                    Console.WriteLine($"Name: {patron.Name}, ID: {patron.ID}");
-                }
-            }
-
-            // Borrow a book for a patron
-            public void BorrowBook(Person patron, string bookTitle)
-            {
-                Book book = Books.Find(b => b.Title.Equals(bookTitle, StringComparison.OrdinalIgnoreCase));
-
-                if (book != null && book.BorrowBook())
-                {
-                    Console.WriteLine($"{patron.Name} borrowed '{book.Title}'");
-                }
-                else
-                {
-                    Console.WriteLine($"{patron.Name} could not borrow '{bookTitle}' (not available).");
-                }
-            }
-
-            // Return a book for a patron
-            public void ReturnBook(Person patron, string bookTitle)
-            {
-                Book book = Books.Find(b => b.Title.Equals(bookTitle, StringComparison.OrdinalIgnoreCase));
-
-                if (book != null)
-                {
-                    book.ReturnBook();
-                    Console.WriteLine($"{patron.Name} returned '{book.Title}'");
-                }
-                else
-                {
-                    Console.WriteLine($"'{bookTitle}' does not belong to this library.");
-                }
-            }
         }
+
+        // Add a book to the library
+
+        public void AddBook(Book book)
+
+        {
+
+            Books.Add(book);
+
+        }
+
+        // Add a patron (student/staff) to the library
+
+        public void AddPatron(Person patron)
+
+        {
+
+            Patrons.Add(patron);
+
+        }
+
+        // Display all books in the library
+
+        public void DisplayBooks()
+
+        {
+
+            Console.WriteLine("\nBooks in Library:");
+
+            foreach (var book in Books)
+
+            {
+
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Available Copies: {book.AvailableCopies}");
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+        // Display all patrons in the library
+
+        public void DisplayPatrons()
+
+        {
+
+            Console.WriteLine("\nPatrons in Library:");
+
+            foreach (var patron in Patrons)
+
+            {
+
+                Console.WriteLine($"Name: {patron.Name}, ID: {patron.id}");
+
+            }
+
+            Console.WriteLine();
+
+        }
+
+        // Borrow a book for a patron
+
+        public void BorrowBook(string patronName, string bookTitle)
+
+        {
+
+            Person patron = Patrons.Find(p => p.Name.Equals(patronName, StringComparison.OrdinalIgnoreCase));
+
+            Book book = Books.Find(b => b.Title.Equals(bookTitle, StringComparison.OrdinalIgnoreCase));
+
+            if (patron != null && book != null && book.BorrowBook())
+
+            {
+
+                Console.WriteLine($"{patron.Name} borrowed '{book.Title}'");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine($"{patronName} could not borrow '{bookTitle}' (not available).");
+
+            }
+
+        }
+
+        // Return a book for a patron
+
+        public void ReturnBook(string patronName, string bookTitle)
+
+        {
+
+            Person patron = Patrons.Find(p => p.Name.Equals(patronName, StringComparison.OrdinalIgnoreCase));
+
+            Book book = Books.Find(b => b.Title.Equals(bookTitle, StringComparison.OrdinalIgnoreCase));
+
+            if (patron != null && book != null)
+
+            {
+
+                book.ReturnBook();
+
+                Console.WriteLine($"{patron.Name} returned '{book.Title}'");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine($"'{bookTitle}' does not belong to this library.");
+
+            }
+
+        }
+
     }
+
 }
+
--- Program.cs
+++ Program.cs
@@ -1,10 +1,77 @@
-﻿namespace USFLibraryMgmtSystem
+﻿using System;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace USFLibraryMgmtSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Staff staff1 = new Staff
+            {
+                Name = "Grandon Gill",
+                EmailId = "grandon@usf.edu",
+                id = "ST001",
+                Position = "Librarian",
+                Department = "Library Services"
+            };
+
+            Student student1 = new Student
+            {
+                Name = "Akhil",
+                EmailId = "akhil@usf.edu",
+                id = "S001",
+                Major = "Business Analytics",
+                GraduationYear = 2026
+            };
+
+            Student student2 = new Student
+            {
+                Name = "Sandeep",
+                EmailId = "sandeep@usf.edu",
+                id = "S002",
+                Major = "Information Systems",
+                GraduationYear = 2025
+            };
+
+            Book book1 = new Book("The Art of Data Strategy", "Liam Reynolds", "ISBN111", 4);
+            Book book2 = new Book("Business Insights with AI", "Olivia Carter", "ISBN222", 3);
+            Book book3 = new Book("Analytics in Action", "Nathan Brooks", "ISBN333", 6);
+
+
+
+            // Create Library
+            Library library = new Library();
+
+
+            // Add Books
+            library.AddBook(book1);
+            library.AddBook(book2);
+            library.AddBook(book3);
+
+            // Add Patrons
+            library.AddPatron(student1);
+            library.AddPatron(student2);
+            library.AddPatron(staff1);
+
+            // Display Initial Books
+            library.DisplayBooks();
+
+            // Display Patrons
+            library.DisplayPatrons();
+
+            // Borrow Books
+            Console.WriteLine("Borrowing Books...");
+            library.BorrowBook("Sandeep", "Business Insights with AI");
+            library.BorrowBook("Akhil", "Analytics in Action");
+            Console.WriteLine();
+
+            // Display Books After Borrowing
+            Console.WriteLine("Books after borrowing:");
+            library.DisplayBooks();
+
         }
     }
 }
