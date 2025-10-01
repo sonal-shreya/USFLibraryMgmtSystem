90e1a3f21617f767d53fd5ffe32c98e9f4bf7e47
sonalshreya25 <sonal.shreya18@gmail.com>
2025-09-30 21:19:51 -0400

    Adding Person Class

---
+++ Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USFLibraryMgmtSystem
+{
+    public class Person
+    {
+        public string Name;
+        public string EmailId;
+        public string id;
+
+
+        public Person()
+        {
+            Name = Name;
+            EmailId = EmailId;
+            id = id;
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+        public void setName(string name)
+        {
+            Name = name;
+
+        }
+        public string getEmailId()
+        {
+            return EmailId;
+        }
+        public void setEmailId(string EmailId)
+        {
+            EmailId = EmailId;
+
+        }
+
+        public string getId()
+        {
+            return id;
+        }
+        public void setId(string id)
+        {
+            id = id;
+        }
+    }
+}
