3651772ae208b13daced42ecc321d87364846c46
sonalshreya25 <sonal.shreya18@gmail.com>
2025-09-30 21:27:15 -0400

    Adding staff class

---
+++ Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USFLibraryMgmtSystem
+{
+    public class Staff: Person
+    {
+        public string Position { get; set; }
+        public string Department { get; set; }
+
+        public Staff()
+        {
+            Position = Position;
+            Department = Department;
+        }
+
+    }
+}
