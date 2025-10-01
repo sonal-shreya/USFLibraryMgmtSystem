dfd6787137ae4f533c6b55e8e88d49aee9dfb501
sumitusf <sumit141@usf.edu>
2025-09-30 21:29:13 -0400

    Adding Student.cs class

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USFLibraryMgmtSystem
+{
+    public class Student : Person
+    {
+        public string Major { get; set; }
+        public int GraduationYear { get; set; }
+
+        public Student()
+        {
+            Major = Major;
+            GraduationYear = GraduationYear;
+        }
+    }
+
+}
